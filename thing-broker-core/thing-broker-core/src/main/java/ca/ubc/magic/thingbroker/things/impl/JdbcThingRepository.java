b556b217d58f89235a30691ead655dc7b0afb906
mike <mblackstock@sensetecnic.com>
2012-03-30 15:09:13 -0700

    Summary: inial thing repository

--- thing-broker-core/thing-broker-core/src/main/java/ca/ubc/magic/thingbroker/things/impl/JdbcThingRepository.java
+++ thing-broker-core/thing-broker-core/src/main/java/ca/ubc/magic/thingbroker/things/impl/JdbcThingRepository.java
@@ -3,7 +3,11 @@
  */
 package ca.ubc.magic.thingbroker.things.impl;
 
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import ca.ubc.magic.thingbroker.things.Thing;
 
 /**
  *
@@ -23,6 +27,10 @@ public class JdbcThingRepository implements ThingRepository {
 	 */
 	public Long getId(String name) {
 		return jdbcTemplate.queryForLong(SELECT_ID_BY_NAME);
+	}
+	
+	public List<Thing> getThings(String name) {
+		return null;
 	}
 	
 	private final static String SELECT_ID_BY_NAME = "";
